d3b845fd9f92a5a8f4d459f8d2ad2836b242e7c0
SKawalec94 <kawalec.szymon@gmail.com>
2019-10-24 15:48:17 +0200

    Podstawowy design

--- index.php
+++ index.php
@@ -16,7 +16,9 @@
         <div class="col-md-8">
             <h1>Hello World!</h1>
             <div class="block">
-                <img src="Moje%20zdjęcie.png" alt="Szymon Kawalec">
+                <div class="photo">
+                    <img src="Moje%20zdjęcie.png" alt="Szymon Kawalec">
+                </div>
             </div>
         </div>
         <div class="col-md-2"></div>
