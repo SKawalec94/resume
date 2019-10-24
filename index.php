2eb3672a9ca706d51530459792b3fe0f37b8f9b5
SKawalec94 <kawalec.szymon@gmail.com>
2019-10-24 17:38:25 +0200

    Podstawowy design

--- index.php
+++ index.php
@@ -4,23 +4,26 @@
     <meta charset="UTF-8">
     <title>Resume</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script></head>
     <link rel="stylesheet" href="resume.css">
 <body>
-
-<div class="flex-container">
-    <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
-            <div class="block">
+<div class="page">
+    <div class="subpage">
+        <div class="block">
+            <div class="photo">
                 <img src="Moje%20zdjęcie.png" alt="Szymon Kawalec">
+            </div>
+            <div class="name">
                 <h1>Szymon Kawalec</h1>
             </div>
         </div>
-        <div class="col-md-2"></div>
     </div>
 </div>
+<div class="page">
+    <div class="subpage">
+        Strona 2
+    </div>
+</div>
+
 </body>
 </html>
