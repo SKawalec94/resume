4cdcdfa22689fd4ba56551be2978642fea9e2dda
SKawalec94 <kawalec.szymon@gmail.com>
2019-10-24 15:17:05 +0200

    Podstawowy design

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script></head>
 <body>
-<h1>Hello World!</h1>
+
+<div class="flex-container">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <h1>Hello World!</h1>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+</div>
 </body>
 </html>
