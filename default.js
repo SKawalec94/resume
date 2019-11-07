aba3a46c706652a4a5af46da5d33caf9683ba975
SKawalec94 <kawalec.szymon@gmail.com>
2019-11-07 18:14:57 +0100

    Resume page

---
+++ default.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#name').css('opacity', '0').css('margin-top', '15vh')
+        .animate({ opacity: 1 }, { duration: 800, queue: false })
+        .animate({ "margin-top": "10vh" }, { duration: 800, queue: false });
+
+    $('.navigation').css('left', '-100px').css('opacity', '0')
+        .animate({ left: 0}, { duration: 800, queue: false })
+        .animate({ opacity: 1 }, { duration: 800, queue: false })
+});
